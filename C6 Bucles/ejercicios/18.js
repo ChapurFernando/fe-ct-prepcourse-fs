79a279120fa179984c1619faacc5c5e0998d400d
FeroChap <fernandochapur27@gmail.com>
2024-09-21 01:06:22 -0300

    Finish C6

--- C6 Bucles/ejercicios/18.js
+++ C6 Bucles/ejercicios/18.js
@@ -2,6 +2,11 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
+  var res = a;
+  for (var i = a + 1; i <= b; i++) {
+    res = res * i;
+  }
+  return Math.abs(res);
 }
 
 module.exports = productoEntreNúmeros;
