d10da64a23bda3c0feb5856b281c4575aaaa0ff2
FeroChap <fernandochapur27@gmail.com>
2024-09-25 19:19:32 -0300

    Finish C8/Callbacks

--- C8 Objects + Callbacks/callbacks/ejercicios/04.js
+++ C8 Objects + Callbacks/callbacks/ejercicios/04.js
@@ -1,9 +1,10 @@
 function sumarArray(arrayOfNumbers, cb) {
-   // Recibes un arreglo de números y un callback.
-   // Suma todos los números del arreglo.
-   // Este resultado debes pasárselo como argumento al callback recibido.
-   // NOTA: no debes retornar nada.
-   // Tu código:
+  // Recibes un arreglo de números y un callback.
+  // Suma todos los números del arreglo.
+  // Este resultado debes pasárselo como argumento al callback recibido.
+  // NOTA: no debes retornar nada.
+  // Tu código:
+  cb(arrayOfNumbers.reduce((acc, num) => acc + num, 0));
 }
 
 module.exports = sumarArray;
